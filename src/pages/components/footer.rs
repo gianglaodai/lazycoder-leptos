31c715789b97c907da0d3cf5c9512428f472578a
giang.hoang <13800000+gianglaodai@users.noreply.github.com>
2025-08-09 23:22:44 +0700

    update leptos to latest version

---
+++ src/pages/components/footer.rs
@@ -0,0 +1,18 @@
+use leptos::prelude::*;
+use time::OffsetDateTime;
+
+#[component]
+pub fn Footer() -> impl IntoView {
+    let year = OffsetDateTime::now_utc().year();
+    view! {
+        <footer class="font-serif bg-[--color-bg]/80 backdrop-blur supports-[backdrop-filter]:bg-[--color-bg]/80 mt-10 px-9 py-8 shadow-[0_0_2em_rgba(0,0,0,0.05)] sm:mx-0 border-t border-black/5">
+            <div class="container-page">
+                <div class="flex items-center justify-center">
+                    <p class="text-lg text-[--color-ink]/80">
+                        {format!("© {} LazyCoder's Blog. All rights reserved.", year)}
+                    </p>
+                </div>
+            </div>
+        </footer>
+    }
+}
